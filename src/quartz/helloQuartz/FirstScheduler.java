27e2dc908e70e6de3abbe65ce12d7ad4ec0c4b7d
penelopeWu <penelopewu98@gmail.com>
2017-09-28 16:39:21 +0800

    add some comments.

--- src/quartz/helloQuartz/FirstScheduler.java
+++ src/quartz/helloQuartz/FirstScheduler.java
@@ -16,23 +16,32 @@ import org.quartz.impl.StdSchedulerFactory;
 public class FirstScheduler {
 	public static void main(String[] args) throws SchedulerException {
 
-		JobDetail jobDetail = JobBuilder.newJob(FirstJob.class).withIdentity("myJob", "group1")
-				.usingJobData("jobMsg", "this is jobDetail message.").build();
+		JobDetail jobDetail = JobBuilder.newJob(FirstJob.class)//获取JobDetail实例，并绑定FisrtJob类
+				.withIdentity("myJob", "group1")//定义Job的名称和组别
+				.usingJobData("jobMsg", "this is jobDetail message.")//使用jobDataMap中的数据
+				.build();
 
 		System.out.println("jobDetail's name: " + jobDetail.getKey().getName());
 		System.out.println("jobDetail's group:" + jobDetail.getKey().getGroup());
 		System.out.println("jobDetail's jobClass : " + jobDetail.getJobClass().getName());
 
 		// 创建一个Trigger实例，定义该Job立即执行，并且每隔2秒重复执行
-		Trigger trigger = TriggerBuilder.newTrigger().withIdentity("myTrigger", "group1")
-				.usingJobData("trMsg", "this is trigger message.").usingJobData("trDouble", 3.0).startNow()
-				.withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(2).repeatForever()).build();
+		Trigger trigger = TriggerBuilder.newTrigger()//过去trigger实例
+				.withIdentity("myTrigger", "group1")//定义trigger的名称和组别
+				.usingJobData("trMsg", "this is trigger message.")//使用jobDataMap中的数据
+				.usingJobData("trDouble", 3.0)//使用jobDataMap中的数据
+				.startNow()
+				.withSchedule(SimpleScheduleBuilder
+						.simpleSchedule()
+						.withIntervalInSeconds(2)
+						.repeatForever())
+				.build();
 
 		// 创建Scheduler实例
 		SchedulerFactory schedulerFactory = new StdSchedulerFactory();
 		Scheduler scheduler = schedulerFactory.getScheduler();
 		scheduler.start();
 		// 执行
-		scheduler.scheduleJob(jobDetail, trigger);
+		scheduler.scheduleJob(jobDetail, trigger);//将JobDetail实例传递给scheduler，其实是把job和trigger告知scheduler.
 	}
 }
