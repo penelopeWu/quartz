3a651168edf3d3cbfc0c9ff17f7ecc38cc16e905
penelopeWu <penelopewu98@gmail.com>
2017-09-28 16:24:06 +0800

    添加注释

--- src/quartz/helloQuartz/FirstScheduler.java
+++ src/quartz/helloQuartz/FirstScheduler.java
@@ -1,4 +1,5 @@
 package quartz.helloQuartz;
+
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
@@ -14,28 +15,19 @@ import org.quartz.impl.StdSchedulerFactory;
  */
 public class FirstScheduler {
 	public static void main(String[] args) throws SchedulerException {
-		
-		JobDetail jobDetail = JobBuilder.newJob(FirstJob.class)
-				.withIdentity("myJob", "group1")
-				.usingJobData("jobMsg","this is jobDetail message.")
-				.build();
-		
+
+		JobDetail jobDetail = JobBuilder.newJob(FirstJob.class).withIdentity("myJob", "group1")
+				.usingJobData("jobMsg", "this is jobDetail message.").build();
+
 		System.out.println("jobDetail's name: " + jobDetail.getKey().getName());
 		System.out.println("jobDetail's group:" + jobDetail.getKey().getGroup());
 		System.out.println("jobDetail's jobClass : " + jobDetail.getJobClass().getName());
-		
+
 		// 创建一个Trigger实例，定义该Job立即执行，并且每隔2秒重复执行
-		Trigger trigger = TriggerBuilder.newTrigger()
-				.withIdentity("myTrigger", "group1")
-				.usingJobData("trMsg","this is trigger message.")
-				.usingJobData("trDouble", 3.0)
-				.startNow()
-				.withSchedule(
-						SimpleScheduleBuilder.simpleSchedule()
-							.withIntervalInSeconds(2)
-							.repeatForever())
-				.build();
-		
+		Trigger trigger = TriggerBuilder.newTrigger().withIdentity("myTrigger", "group1")
+				.usingJobData("trMsg", "this is trigger message.").usingJobData("trDouble", 3.0).startNow()
+				.withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(2).repeatForever()).build();
+
 		// 创建Scheduler实例
 		SchedulerFactory schedulerFactory = new StdSchedulerFactory();
 		Scheduler scheduler = schedulerFactory.getScheduler();
